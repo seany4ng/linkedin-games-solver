5efaafa5c32dc6f5d02391c40a4f53b2ab3340cb
Sean Yang <seanyang12345@gmail.com>
2024-12-05 13:09:05 -0800

    add structure

---
+++ tango/web/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  // TODO: this is the entrypoint for our app. we can start with Tango.
+  return <h1>Hello, world!</h1>;
+};
+
+export default App;
