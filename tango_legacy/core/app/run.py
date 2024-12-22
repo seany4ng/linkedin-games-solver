19327a0405d44abea5e9d8aa2d822051ff5fa1b9
Sean Yang <seanyang12345@gmail.com>
2024-12-22 11:17:49 -0800

    delete legacy code

--- tango_legacy/core/app/run.py
+++
@@ -1,6 +0,0 @@
-from core.app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
--- tango_legacy/core/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from core.app import create_app
-
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config['TESTING'] = True  # Enables Flask's testing mode
-    client = app.test_client()
-    yield client
